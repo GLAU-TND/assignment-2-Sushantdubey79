a0fdbd2a287640c0b1ab20bd7c274d5f2197159c
Sushantdubey79 <sushantdubey7607@gmail.com>
2020-03-28 19:18:01 +0530

    problem 1 added

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,25 @@
 package problem1.main;
 // executable class
 
+
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree mytree = new MyBinarySearchTree();
+        mytree.insert(50);
+        mytree.insert(30);
+        mytree.insert(20);
+        mytree.insert(40);
+        mytree.insert(70);
+        mytree.insert(60);
+        mytree.insert(80);
+        mytree.insert(90);
+        mytree.insert(10);
+        System.out.println("PreOrder Traversal : ");
+        mytree.traversePreOrder();
+        System.out.println("Left children Only : ");
+        System.out.println(mytree.traverseLeft());
+
+    }
 }
